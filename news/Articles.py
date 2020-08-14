da760e57664a7b99e493471c74ed64eae0d8872b
jehill <jehillparikh@gmail.com>
2020-08-14 12:00:27 +0100

    Initial commit

---
+++ news/Articles.py
@@ -0,0 +1,35 @@
+from news.News import *
+from news.Stocks import getstocktickers
+
+
+if __name__ == '__main__':
+
+    stocks=getstocktickers()
+    print(stocks.head(5))
+    stocks=stocks.head(200)
+    stocks["articles"]=stocks["Symbol"].map(lambda ticker: get_news_urls_nasdaq(ticker))
+    stocks.to_json("Nasdaq_articles")
+
+    #print(stocks.head(3))
+    #print(articles["articles"].head(5))
+    #articles["NewsContent"]=articles["articles"].map(lambda urls: parse_urls_text(urls))
+
+    articles=pd.read_json("Nasdaq_articles")
+    articles["Newscontent"]= ''
+    newdata=[]
+
+    for i, row in articles.iterrows():
+
+        print(i)
+
+
+
+        content=parse_urls_text(row["articles"], row["Symbol"])
+        articles.at[i, "Newscontent"]=content
+        newdata.append(content)
+
+        print(content)
+
+
+    articles["Newscontent"]=newdata
+    articles.to_json("Nasdaq_newsContent")
---
+++ news/Stocks.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def getstocktickers():
+
+    table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    df = table[0]
+    colsNames={0: 'Symbol', 1: 'Security' , 2 : 'SEC filings', 3 : 'GICS Sector', 4 : 'GICS Sub Industry',
+               5 : 'Headquarters Location', 6 : 'Date first added' , 7: 'CIK' , 8: 'Founded' }
+
+
+    df=df.drop([0])  #first rown contains column names do just drop it
+    df= df.rename(columns=colsNames)
+
+    return df
+
+
+if __name__ == '__main__':
+    stocks=getstocktickers()
+
+
