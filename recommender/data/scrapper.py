2dd7804e5543a7c7d816cc41ab39d72e51c1bd4b
Jehill Parikh <jehillparikh@gmail.com>
2020-09-22 09:26:24 +0100

    adding models

---
+++ recommender/data/scrapper.py
@@ -0,0 +1,8 @@
+from linkedin_scraper import Person, actions
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+email = "some-email@email.address"
+password = "password123"
+actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)
